23be08beed174b3a9d14a9ffdf723eec4c16bf4d
Daiu Szwimer <32345857+daianaszwimer@users.noreply.github.com>
2022-09-12 20:02:38 -0300

    Update index.tsx

--- app/routes/__app/index.tsx
+++ app/routes/__app/index.tsx
@@ -43,6 +43,7 @@ export const action: ActionFunction = async({ request }) => {
   const body = await request.formData();
   try {
     const text = body.get("problem") as string;
+    console.log(process.env.API_URL, "env---aaaa---");
     const mathExpression = await fetch(`${process.env.API_URL}/math-translation`, {
       method: "POST",
       body: JSON.stringify({ text }),
@@ -50,7 +51,7 @@ export const action: ActionFunction = async({ request }) => {
         "Content-Type": "application/json"
       }
     });
-    console.log(process.env.API_URL, "env------");
+    console.log(process.env.API_URL, "env---aaaa---");
     const result = await mathExpression.json();
     if (result.error || result.result === "") {
       return json<ActionData>({
