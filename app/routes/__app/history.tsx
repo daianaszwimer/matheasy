ababc4fd78c9dfa2ae7537e0e476e8baddc5497e
Daiana Szwimer <daiu.szwimer@gmail.com>
2022-10-08 18:24:57 -0300

    feat: usar otra font, sacar bold, agregar ver todo, sacar emojis, sacar un poco de padding, fixear doble click

--- app/routes/__app/history.tsx
+++ app/routes/__app/history.tsx
@@ -8,23 +8,24 @@ export default function History() {
     setHistory(_history);
   }, []);
   return (<>
-    <h1 className="text-2xl md:text-3xl font-bold text-white text-center">
+    <h1 className="text-2xl md:text-3xl text-white text-center">
+      <span className="mr-2" aria-hidden>&#128214;</span>
       Historial de enunciados
     </h1>
     {history.length > 0 ?
       <>
-        <h2 className="text-md md:text-xl font-bold text-white text-center">
+        <h2 className="text-base md:text-xl text-white text-center">
           ¡Hacé clic en el enunciado que quieras repasar!
         </h2>
-        <ul className="list-disc space-y-6">
+        <ul className="space-y-6">
           {history.reverse().map(element => (
             <li key={element}>
-              <Link className="underline" to={`/?text=${encodeURI(element.replace("+", "%2B"))}`}>{element}</Link>
+              <Link className="underline" to={`/?index&text=${encodeURI(element.replace("+", "%2B"))}`}>{element}</Link>
             </li>
           ))}
         </ul>
       </> :
-      <h2 className="text-xl font-bold text-white text-center">
+      <h2 className="text-xl text-white text-center">
         ¡Nada por aquí! A medida que resuelvas ejercicios tu historial crecerá
       </h2>}
   </>);
