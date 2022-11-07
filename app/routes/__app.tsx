958f199c0535201cb6a87898464ac2d5e73a4adb
Daiana Szwimer <daiu.szwimer@gmail.com>
2022-11-07 00:11:43 -0300

    mejorar navegacion

--- app/routes/__app.tsx
+++ app/routes/__app.tsx
@@ -1,5 +1,5 @@
 import icon from "~/assets/icon.png";
-import { Link, Outlet } from "@remix-run/react";
+import { NavLink, Outlet } from "@remix-run/react";
 
 export function links() {
   return [
@@ -10,14 +10,29 @@ export function links() {
   ];
 }
 
+function Link({ to, text, reload = false }:
+ {to: string, text: string, reload?: boolean}) {
+  return (
+    <NavLink
+      className={({ isActive }) =>
+        isActive ? "p-4 pb-3.5 border-b-2 border-white" : "p-4"
+      }
+      to={to}
+      reloadDocument={reload}
+    >
+      {text}
+    </NavLink>
+  );
+}
+
 export default function App() {
   return (
     <div className="bg-gray-900 font-['Comfortaa'] text-white main-background bg-repeat overflow-hidden">
-      <nav className="bg-slate-800 border-b-2 border-gray-700">
+      <nav className="bg-slate-800">
         <ul className="flex gap-2 md:gap-6 px-3 items-center">
-          <li className="flex"><Link className="p-4 underline" to="/" reloadDocument>Inicio</Link></li>
-          <li className="flex"><Link className="p-4 underline" to="/history">Historial</Link></li>
-          <li className="flex"><Link className="p-4 underline" to="/faq">Ayuda</Link></li>
+          <li className="flex"><Link to="/" text="Inicio" reload/></li>
+          <li className="flex"><Link to="/history" text="Historial" reload/></li>
+          <li className="flex"><Link to="/faq" text="Ayuda" reload/></li>
         </ul>
       </nav>
       <main className="min-h-[calc(100vh_-_32px_-_16px_-_16px_-_56px)] md:space-y-6 space-y-4 flex-col md:pt-10 pt-7 p-5 lg:px-0 sm:max-w-3xl sm:mx-auto">
