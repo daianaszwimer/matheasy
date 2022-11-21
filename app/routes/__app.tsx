fb96303ba9271f50fa64ed44d53e66b3a96fb4d4
Daiana Szwimer <daiu.szwimer@gmail.com>
2022-11-20 21:34:25 -0300

    cambio ux y reemplazo usefetcher por usesubmit

--- app/routes/__app.tsx
+++ app/routes/__app.tsx
@@ -1,15 +1,14 @@
 import icon from "~/assets/icon.png";
 import { NavLink, Outlet } from "@remix-run/react";
 
-function NavItem({ to, text, reload = false }:
- {to: string, text: string, reload?: boolean}) {
+function NavItem({ to, text }:
+ {to: string, text: string}) {
   return (
     <NavLink
       className={({ isActive }) =>
-        isActive ? "p-4 pb-3.5 border-b-2 border-white font-medium" : "p-4"
+        isActive ? "p-4 pb-3.5 border-b-2 border-white font-medium" : "my-0.5 py-3.5 px-4 transition-colors hover:bg-gray-700 rounded-md"
       }
       to={to}
-      reloadDocument={reload}
     >
       {text}
     </NavLink>
@@ -21,9 +20,22 @@ export default function App() {
     <div className="bg-gray-900 font-['Nunito'] text-white main-background bg-repeat overflow-hidden">
       <nav className="bg-slate-800">
         <ul className="flex gap-2 md:gap-6 px-3 items-center">
-          <li className="flex"><NavItem to="/" text="Inicio" reload/></li>
-          <li className="flex"><NavItem to="/history" text="Historial" reload/></li>
-          <li className="flex"><NavItem to="/faq" text="Ayuda" reload/></li>
+          <li className="flex">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "p-3.5 pb-3 border-b-2 border-white font-medium" : "my-0.5 py-3 px-3.5 transition-colors hover:bg-gray-700 rounded-md"
+              }
+              to="/"
+            >
+              <img width={28} src={icon} alt="Logo de MathEasy"/>
+            </NavLink>
+          </li>
+          <li className="flex">
+            <NavItem to="/history" text="Historial"/>
+          </li>
+          <li className="flex">
+            <NavItem to="/faq" text="Ayuda"/>
+          </li>
         </ul>
       </nav>
       <main className="min-h-[calc(100vh_-_32px_-_16px_-_16px_-_56px)] md:space-y-6 space-y-4 flex-col md:pt-10 pt-7 p-5 lg:px-0 sm:max-w-3xl sm:mx-auto">
